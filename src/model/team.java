9bfd313f04043809e0264fb74c7077f0878c93e8
ursula Paul <ursulapaul31@gmail.com>
2025-08-25 07:27:09 +0100

    Updated the TeamModel

--- src/model/team.java
+++ src/model/team.java
@@ -2,42 +2,41 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Team {
-
-    Team(){
-        
-    }
     private String name;
     private List<Player> players;
 
     public Team(String name) {
-        this.name = name;
+        this.name =name;
         this.players = new ArrayList<>();
     }
 
-    public String getName() {
+    public String getname(){
         return name;
     }
 
-    public List<Player> getPlayers() {
+    public List<Player>getPlayers(){
         return players;
     }
 
-    public boolean addPlayer(Player player) {
-        if (players.size() < 8) {
+    public boolean addPlayer (Player player){
+        if (players.size()<8){
             return players.add(player);
 
-        } else {
-            System.out.println("Team already has 8 players. cannot add more");
+        }else{
+            System.out.println("Team already has 8 players. Cannot add more")
             return false;
         }
     }
 
-    public boolean removePlayer(String fullName) {
-        return players.removeIf(p -> p.getFullName().equalsIgnoreCase(fullName));
+    public boolean removePlayer(String fullName){
+         return players.removeIf(p->p.getFullName().equalsIgnoreCase(fullName));
     }
 
     @Override
-    public String toString() {
-        return "Team " + name + "(" + players.size() + " players)";
+    public String toString(){
+        return "Team " + name + " (" + players.size() + " players)";
     }
-}
+
+    }
+
+
